87799868f20425290a090f98e9868e77fe8ce7a0
OluSure <tobisco29@gmail.com>
2025-09-17 15:42:01 +0100

    first version

--- app.js
+++ app.js
@@ -77,11 +77,11 @@ function clearList() {
 function searchExpense() {
   let expenses = JSON.parse(localStorage.getItem("expenses"));
   let table = document.getElementById("expense-list");
-  table.innerHTML = "";
+  table.innerHTML = "<thead><tr><th>Amount</th><th>Category</th><th>Date</th><th>Action</th></tr></thead>";
   let total = 0;
   for (let i = 0; i < expenses.length; i++) {
     if (
-        (amount.value === "" || expenses[i].amount).toLowerCase().includes(amount.value) &&
+        (amount.value === "" ||String(expenses[i].amount).toLowerCase().includes(amount.value.toLowerCase())) &&
        (category.value === "" || String(expenses[i].category).toLowerCase().includes(category.value.toLowerCase())) &&
        (date.value === "" || String(expenses[i].date).toLowerCase().includes(date.value.toLowerCase()))
             ) {
@@ -90,7 +90,7 @@ function searchExpense() {
       let cell2 = row.insertCell(1);
       let cell3 = row.insertCell(2);
       let cell4 = row.insertCell(3);
-      cell1.innerHTML = expenses[i].amount;
+      cell1.innerHTML = "₦" + expenses[i].amount;
       cell2.innerHTML = expenses[i].category;
       cell3.innerHTML = expenses[i].date;
       cell4.innerHTML = "<button onclick='deleteExpense(" + i + ")'>Delete</button>";
@@ -106,7 +106,7 @@ function searchExpense() {
   cell1.innerHTML = "Total";
   cell2.innerHTML = "";
   cell3.innerHTML = "";
-  cell4.innerHTML = total;
+  cell4.innerHTML = "₦" + total;
 }
 
 // load expenses from local storage
